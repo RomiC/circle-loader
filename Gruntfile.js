534594bad973834e96db941f0ed93c29292f6e1f
Roman Charugin <i@charugin.ru>
2013-10-05 11:23:19 +0400

    Minor updates of grunt files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,21 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			uglified: {
 				files: {
 					'js/jquery.circle-loader.min.js': ['js/jquery.circle-loader.js']
 				}
 			}
+		},
+		watch: {
+			js: {
+				files: ['js/jquery.circle-loader.js'],
+				tasks: ["uglify"]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['uglify']);
 };
